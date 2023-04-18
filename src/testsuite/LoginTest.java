2e54461646402e488434ee140d11463378097bf0
dpdesai02 <dpdesai2884@gmail.com>
2023-04-18 21:15:14 +0100

    com-nopcommerce-sw-2-dhara

---
+++ src/testsuite/LoginTest.java
@@ -0,0 +1,33 @@
+package testsuite;
+
+import broeserfactory.BaseTest;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+
+public class LoginTest extends BaseTest {
+    String baseUrl = " https://demo.nopcommerce.com/ ";
+
+
+    @Before
+    public void setUp() {
+        openBrowser(baseUrl);
+    }
+
+    public void LoginTest() {
+        //Relative path
+        driver.findElement(By.id("Email")).sendKeys("abcdefg@gmail.com");
+        //find password
+        driver.findElement(By.name("Password")).sendKeys("AimHigh123");
+
+        WebElement loginBoton = driver.findElement(By.xpath(""));
+    }
+
+    @After
+    public void tearDown() {
+        closeBrowser();
+    }
+
+}
